72a8284287ed18c2fdc168a1be8f15af48b7c21f
therahul28 <therahulyadav0077@gmail.com>
2023-04-03 15:45:36 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,35 @@
+#Module      : labels
+#Description : This terraform module is designed to generate consistent label names and tags
+#              for resources. You can use terraform-labels to implement a strict naming
+#              convention.
+module "labels" {
+  source      = "git::git@github.com:slovink/terraform-aws-labels.git"
+  name        = var.name
+  repository  = var.repository
+  environment = var.environment
+  managedby   = var.managedby
+  label_order = var.label_order
+}
+
+# Module      : KMS KEY
+# Description : This terraform module creates a KMS Customer Master Key (CMK) and its alias.
+resource "aws_kms_key" "default" {
+  count                    = var.enabled ? 1 : 0
+  description              = var.description
+  key_usage                = var.key_usage
+  deletion_window_in_days  = var.deletion_window_in_days
+  is_enabled               = var.is_enabled
+  enable_key_rotation      = var.enable_key_rotation
+  customer_master_key_spec = var.customer_master_key_spec
+  policy                   = var.policy
+  multi_region             = var.multi_region
+  tags                     = module.labels.tags
+}
+
+# Module      : KMS ALIAS
+# Description : Provides an alias for a KMS customer master key..
+resource "aws_kms_alias" "default" {
+  count         = var.enabled ? 1 : 0
+  name          = coalesce(var.alias, format("alias/%v", module.labels.id))
+  target_key_id = join("", aws_kms_key.default.*.id)
+}
